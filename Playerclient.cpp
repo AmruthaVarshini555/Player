04bc5f2c8876cad5b69d6f549b5e8e874659e573
AmruthaVarshini555 <119595744+AmruthaVarshini555@users.noreply.github.com>
2023-01-04 11:43:14 +0530

    Create Playerclient.cpp

---
+++ Playerclient.cpp
@@ -0,0 +1,10 @@
+#include"Player.h"
+int main(){
+        Player p1;
+        p1.accept();
+        p1.display();
+        cout<<"\n----------------------\n";
+        Player p2("Harish",28);
+        p2.display();
+        return 0;
+}
